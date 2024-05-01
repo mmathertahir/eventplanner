19ab8d49fe29119d950e327868eb00b52bf1545e
Muhammad Mather <mianmather249@gmail.com>
2024-05-01 13:03:23 +0500

    Tailwind Config File Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
   ],
+
   theme: {
     screens: {
       sm: "640px",
